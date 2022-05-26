10fa77a4fdaa382e4d44e7d0b2e7c1cef6d39737
Rolando Martinez <itsupportmx@ctscargo.net>
2022-05-26 13:48:10 -0500

    se termina de hacer el proyecto de dibujar el cerdo en la granja

--- videojuego/platzi.js
+++ videojuego/platzi.js
@@ -1,20 +1,34 @@
+document.addEventListener("keydown", moverCerdo);
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
+var cantidad = aleatorio(2, 10);
+var teclas = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
 var fondo = {
   url: "tile.png",
   cargaOk: false
 };
 var vaca = {
   url: "vaca.png",
-  cargaOk: false
+  cargaOk: false,
+  x : [],
+  y : []
 };
 var pollo = {
   url: "pollo.png",
-  cargaOk: false
+  cargaOk: false,
+  x : [],
+  y : []
 };
 var cerdo = {
   url: "cerdo.png",
-  cargaOk: false
+  cargaOk: false,
+  x : 0,
+  y : 0
 };
 
 fondo.imagen = new Image();
@@ -38,36 +52,82 @@ function dibujar()
   }
   if (vaca.cargaOk)
   {
-    var cantidad = aleatorio(2, 10);
-    console.log("vacas: " + cantidad);
     for (var i = 0; i < cantidad; i++)
     {
       var x = aleatorio(0, 6) * 70;
       var y = aleatorio(0, 6) * 70;
+      vaca.x[i] = x;
+      vaca.y[i] = y;
       papel.drawImage(vaca.imagen,x , y);
     }
   }
   if (pollo.cargaOk)
   {
-    var cantidad = aleatorio(2, 10);
-    console.log("pollos: " + cantidad);
     for (var i = 0; i < cantidad; i++)
     {
       var x = aleatorio(0, 6) * 70;
       var y = aleatorio(0, 6) * 70;
+      pollo.x[i] = x;
+      pollo.y[i] = y;
       papel.drawImage(pollo.imagen,x , y);
     }
   }
-  if (cerdo.cargaOk)
+}
+
+function dibujarDeNuevo ()
+{
+  if (fondo.cargaOk)
   {
-    var cantidad = aleatorio(2, 10);
-    console.log("cerdos: " + cantidad);
+    papel.drawImage(fondo.imagen, 0 , 0);
+  }
+  if (vaca.cargaOk)
+  {
     for (var i = 0; i < cantidad; i++)
     {
-      var x = aleatorio(0, 6) * 70;
-      var y = aleatorio(0, 6) * 70;
-      papel.drawImage(cerdo.imagen,x , y);
+      papel.drawImage(vaca.imagen,vaca.x[i] ,vaca.y[i]);
     }
+  }
+  if (pollo.cargaOk)
+  {
+    for (var i = 0; i < cantidad; i++)
+    {
+      papel.drawImage(pollo.imagen,pollo.x[i] ,pollo.y[i]);
+    }
+  }
+}
+
+function teclasCerdo (x,y)
+{
+  papel.drawImage(cerdo.imagen,x,y);
+}
+
+function moverCerdo(evento)
+{
+  var movimiento = 5;
+  switch (evento.keyCode) {
+    case teclas.DOWN:
+      dibujarDeNuevo();
+      cerdo.y = cerdo.y + movimiento;
+      teclasCerdo(cerdo.x,cerdo.y);
+    break;
+    case teclas.UP:
+      dibujarDeNuevo();
+      cerdo.y = cerdo.y - movimiento;
+      teclasCerdo(cerdo.x,cerdo.y);
+    break;
+    case teclas.LEFT:
+      dibujarDeNuevo();
+      cerdo.x = cerdo.x - movimiento;
+      teclasCerdo(cerdo.x,cerdo.y);
+    break;
+    case teclas.RIGHT:
+      dibujarDeNuevo();
+      cerdo.x = cerdo.x + movimiento;
+      teclasCerdo(cerdo.x,cerdo.y);
+    break;
+    default:
+      console.log("otra tecla");
+
   }
 }
 
